cd28736a21895079c41097ce67c875df52008c54
Andy South <southandy@gmail.com>
2014-12-11 18:51:16 +0000

    tidy

--- rmd2md.r
+++ rmd2md.r
@@ -51,7 +51,6 @@ rmd2md <- function( path_site = getwd(),
           outFile <- file.path(path_site, dir_md, paste0(substr(f, 1, (nchar(f)-(nchar(in_ext)))), out_ext))
                    
           #render_markdown(strict=TRUE)
-
           #render_markdown(strict=FALSE) #code didn't render properly on blog
           
           #andy change to render for jekyll
